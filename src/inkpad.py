2b7ca3e0a796c83e6556c759dfef420115012f4b
Soham Biswas <sohambiswas41@gmail.com>
2021-01-01 12:24:25 +0530

    add open file event

--- src/inkpad.py
+++ src/inkpad.py
@@ -7,12 +7,15 @@ class Inkpad(QtWidgets.QMainWindow, Ui_main_window):
         self.setupUi(self)
 
         self.actionNew.triggered.connect(self.new_file_event)
+        self.actionOpen.triggered.connect(self.open_file_event)
 
     def new_file_event(self):
         new_tab = QtWidgets.QWidget()
         gridLayout = QtWidgets.QGridLayout(new_tab)
         new_text_edit = QtWidgets.QPlainTextEdit(new_tab)
-        new_text_edit.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.IBeamCursor))
+        new_text_edit.viewport().setProperty("cursor",
+                                             QtGui.QCursor(QtCore.Qt
+                                                           .IBeamCursor))
         new_text_edit.setMouseTracking(False)
         new_text_edit.setDocumentTitle("")
         new_text_edit.setPlainText("")
@@ -22,6 +25,14 @@ class Inkpad(QtWidgets.QMainWindow, Ui_main_window):
         gridLayout.addWidget(new_text_edit, 0, 0, 1, 2)
         self.content_tab.addTab(new_tab, "Untitled*")
 
+    def open_file_event(self):
+        file_path, _ = (QtWidgets.QFileDialog
+                        .getOpenFileName(self, "Open File",
+                                         QtCore.QDir.homePath(),
+                                         "All Files (*);;Text Files (*.txt)"))
+
+        with open(file_path, 'r') as opened_file:
+            self.content_edit.setPlainText(opened_file.read())
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
