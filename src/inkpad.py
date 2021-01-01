7842c7a14c7646f755fb4196652b832dd09f046c
Soham Biswas <sohambiswas41@gmail.com>
2021-01-01 12:56:27 +0530

    add save file trigger

--- src/inkpad.py
+++ src/inkpad.py
@@ -8,6 +8,7 @@ class Inkpad(QtWidgets.QMainWindow, Ui_main_window):
 
         self.actionNew.triggered.connect(self.new_file_event)
         self.actionOpen.triggered.connect(self.open_file_event)
+        self.actionSave.triggered.connect(self.save_file_event)
 
     def new_file_event(self):
         new_tab = QtWidgets.QWidget()
@@ -31,8 +32,27 @@ class Inkpad(QtWidgets.QMainWindow, Ui_main_window):
                                          QtCore.QDir.homePath(),
                                          "All Files (*);;Text Files (*.txt)"))
 
-        with open(file_path, 'r') as opened_file:
-            self.content_edit.setPlainText(opened_file.read())
+        if file_path != "":
+            with open(file_path, 'r') as opened_file:
+                self.content_edit.setPlainText(opened_file.read())
+
+            self.content_tab.setTabText(self.content_tab.currentIndex(),
+                                        file_path.split('/')[-1])
+
+
+    def save_file_event(self):
+        file_path, _ = (QtWidgets.QFileDialog
+                        .getSaveFileName(self,"Save File",
+                                         QtCore.QDir.homePath(),
+                                         "All Files (*);;Text Files (*.txt)"))
+
+        if file_path != "":
+            with open(file_path, 'w') as saved_file:
+                saved_file.write(self.content_edit.toPlainText())
+
+            self.content_tab.setTabText(self.content_tab.currentIndex(),
+                                        file_path.split('/')[-1])
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
