8ab015c2f7bc47793a2cd567c40d80d3cd347a90
Soham Biswas <sohambiswas41@gmail.com>
2021-01-03 15:25:40 +0530

    add some test changes

--- tests/test_inkpad.py
+++ tests/test_inkpad.py
@@ -18,9 +18,30 @@ def test_default_tab(inkpad_app):
     assert inkpad_app.content_tab.currentIndex() == 0
     assert inkpad_app.content_tab.tabText(0) == "Untitled*"
 
-# def test_new_file_trigger(inkpad_app, qtbot):
+
+def test_new_file_trigger(inkpad_app, qtbot):
+    prev_tab_count = inkpad_app.content_tab.count()
+    inkpad_app.show()
+    qtbot.waitForWindowShown(inkpad_app)
+
+    qtbot.keyClick(inkpad_app.content_tab.currentWidget(),
+                   QtCore.Qt.Key_N, modifier=QtCore.Qt.ControlModifier)
+
+    curr_tab_count = inkpad_app.content_tab.count()
+    assert curr_tab_count - prev_tab_count == 1
+    assert inkpad_app.content_tab.tabText(1) == "Untitled*"
+
+# def test_remove_tab(inkpad_app, qtbot):
+#     inkpad_app.content_tab.tab
+
+
+# def test_open_file_trigger(inkpad_app, qtbot):
 #     prev_tab_count = inkpad_app.content_tab.count()
-#     # qtbot.mouseClick(inkpad_app.actionNew, QtCore.Qt.LeftButton)
-#     qtbot.keyClick(inkpad_app.content_tab.currentWidget(), QtCore.Qt.Key_N, modifier=QtCore.Qt.ControlModifier)
+#     inkpad_app.show()
+#     qtbot.waitForWindowShown(inkpad_app)
+
+#     qtbot.keyClick(inkpad_app.content_tab.currentWidget(),
+#                    QtCore.Qt.Key_O, modifier=QtCore.Qt.ControlModifier)
+
 #     curr_tab_count = inkpad_app.content_tab.count()
 #     assert curr_tab_count - prev_tab_count == 1
